6e5a540269b2453cb083cd9648cec881917b47b0
SomegnihT <somegniht@gmail.com>
2026-04-05 14:34:56 -0600

    Starting work on getting this shit to work hopefully

---
+++ Content.Server/_Box/ConsentBeRRTarget/Components/ConsentBeRRTarget.cs
@@ -0,0 +1,10 @@
+namespace Content.Server._Box.ConsentBeRRTarget.Components;
+
+/// <summary>
+/// Given to those who have Round Remove Target Consent on.  Used for assigning traitors to kill those who have Round Remove Target Consent on.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ConsentBeRRTargetComponent : Component
+{
+
+}
